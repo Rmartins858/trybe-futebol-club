8d3e000fc44cf056d14619e8af50d1f004a4e4eb
Rafael Martins <rmartins858@gmail.com>
2022-10-27 19:25:53 -0300

    feat: teamns implementation req 26

--- app/backend/src/middlewares/Auth.Matches.ts
+++ app/backend/src/middlewares/Auth.Matches.ts
@@ -1,12 +1,32 @@
 import { NextFunction, Request, Response } from 'express';
 import HttpException from '../shared/http.exeption';
+import TeamsService from '../service/ClubService';
 
-const authMatches = async (req: Request, res: Response, next: NextFunction) => {
-  const { awayTeam, homeTeam } = req.body;
-  if (homeTeam === awayTeam) {
-    throw new HttpException(422, 'It is not possible to create a match with two equal teams');
+export default class AuthMatches {
+  teamsService: TeamsService;
+
+  constructor() {
+    this.teamsService = new TeamsService();
   }
-  next();
-};
 
-export default authMatches;
+  authMatches = async (req: Request, res: Response, next: NextFunction) => {
+    const { awayTeam, homeTeam } = req.body;
+
+    const existingTeams = await this.teamsService.getClubById(
+      awayTeam,
+      homeTeam,
+    );
+
+    if (!existingTeams) {
+      throw new HttpException(404, 'There is no team with such id!');
+    }
+
+    if (homeTeam === awayTeam) {
+      throw new HttpException(
+        422,
+        'It is not possible to create a match with two equal teams',
+      );
+    }
+    next();
+  };
+}
--- app/backend/src/service/ClubService.ts
+++ app/backend/src/service/ClubService.ts
@@ -21,6 +21,13 @@ class TeamsService {
     }
     return teamsById;
   };
+
+  getClubById = async (homeTeam: number, awayTeam: number) => {
+    const homeTeamId = await this.model.findOne({ where: { id: homeTeam } });
+    const awayTeamId = await this.model.findOne({ where: { id: awayTeam } });
+    const teamOk = !(!homeTeamId || !awayTeamId);
+    return teamOk;
+  };
 }
 
 export default TeamsService;
